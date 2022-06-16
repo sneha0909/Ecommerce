6a38b29aafeef403612c039ea78aee5db4b7a838
Sneha Shukla <snehadefence090998@gmail.com>
2022-06-16 08:12:29 +0700

    End of section 6

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { StoreProvider } from './app/context/StoreContext';
 
 export const history = createBrowserHistory();
 
@@ -12,13 +13,17 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+
+
   <Router history={history}>
     <React.StrictMode>
-
-      <App />
-
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </React.StrictMode>
   </Router>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
