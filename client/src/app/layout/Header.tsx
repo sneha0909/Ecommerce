3f87d3c688aaf8b44935e18fce48023e9f852930
Sneha Shukla <snehadefence090998@gmail.com>
2022-06-20 23:29:13 +0700

    End of section 9

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
     darkMode: boolean;
@@ -32,11 +33,12 @@ const navStyles = {
     }
 
 }
-export default function Header({ darkMode, handleThemeChange}: Props) {
-    const {basket} = useAppSelector(state => state.basket);
+export default function Header({ darkMode, handleThemeChange }: Props) {
+    const { basket } = useAppSelector(state => state.basket);
+    const { user } = useAppSelector(state => state.account)
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
     return (
-        <AppBar position='static' sx={{mb: 4}}>
+        <AppBar position='static' sx={{ mb: 4 }}>
             <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Box display='flex' alignItems='center'>
 
@@ -50,7 +52,7 @@ export default function Header({ darkMode, handleThemeChange}: Props) {
 
                 </Box>
 
-                <List sx={{display: 'flex' }}>
+                <List sx={{ display: 'flex' }}>
                     {midLinks.map(({ title, path }) => (
                         <ListItem
                             component={NavLink}
@@ -70,18 +72,24 @@ export default function Header({ darkMode, handleThemeChange}: Props) {
 
                         </Badge>
                     </IconButton>
-                    <List sx={{ display: 'flex' }}>
-                        {rightLinks.map(({ title, path }) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? (
+                        <SignedInMenu />
+                    ) : (
+                        <List sx={{ display: 'flex' }}>
+                            {rightLinks.map(({ title, path }) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+
+                    )}
+
                 </Box>
 
             </Toolbar>
--- client/src/app/layout/LoadingComponent.tsx
+++ client/src/app/layout/LoadingComponent.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function LoadingComponent({message ='Loading...'}: Props) {
     return (
-        <Backdrop open={true}> invisible={true}
+        <Backdrop open={true} invisible={true}>
            <Box display='flex' justifyContent='center' alignItems='center' height='100vh'>
                <CircularProgress size={100} color='secondary' />
                <Typography variant='h4' sx={{justifyContent: 'center', position: 'fixed', top: '60%'}}>{message}</Typography>
