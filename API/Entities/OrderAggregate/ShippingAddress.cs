45770b87cfe8787cff21e083ee943b238654cde4
Sneha Shukla <snehadefence090998@gmail.com>
2022-06-22 23:37:41 +0700

    Implementation of Checkout feature
    In the frontend, created checkout form where user fills the required information regarding the order ad proceeds to checkout. Also, user can save the address on the server as its default address.

---
+++ API/Entities/OrderAggregate/ShippingAddress.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities.OrderAggregate
+{
+    [Owned]
+    public class ShippingAddress : Address
+    {
+
+        
+    }
+}
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -20,6 +20,7 @@ import Register from "../../features/account/Register";
 import Login from "../../features/account/Login";
 import { fetchCurrentUser } from "../../features/account/accountSlice";
 import PrivateRoute from "./PrivateRoute";
+import Orders from "../../features/orders/Orders";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -71,6 +72,7 @@ function App() {
           <Route path='/server-error' component={ServerError} />
           <Route path='/basket' component={BasketPage} />
           <PrivateRoute path='/checkout' component={CheckoutPage} />
+          <PrivateRoute path='/orders' component={Orders} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
           <Route component={NotFound} />
